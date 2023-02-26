82cb519a93f37f345d1006fb9cb8784945e87860
Maksim Usmanov | Maks <skamasle@gmail.com>
2023-02-26 14:08:51 +0100

    Create borg.sh

---
+++ borg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# store this in $HESTIA/func
+BORGREPOS=/home/backup/
+BORGENCRYPT=none
+BORGRFORMAT=$(date +%F-%T)
+# Web 
+KEEPWEBDAILY=7
+KEEPWEBWEEKLY=4
+# Mail 
+#Want Store mails more time than web backup ? its posible
+KEEPMAILDAILY=7
+KEEPMAILWEEKLY=4
